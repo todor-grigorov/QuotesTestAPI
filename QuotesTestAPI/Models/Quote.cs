9f09393feb236c56dabd0a546edb8148076bcddb
todor-grigorov <t.grigorov99@gmail.com>
2023-07-10 21:54:09 +0200

    feat: add quote model

---
+++ QuotesTestAPI/Models/Quote.cs
@@ -0,0 +1,10 @@
+﻿namespace QuotesTestAPI.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
