0fc6a13050a23c90d0257e0576c9b48704ff417a
todor-grigorov <t.grigorov99@gmail.com>
2023-07-10 21:59:09 +0200

    feat: add QuoteRepository

---
+++ QuotesTestAPI/Repository/QuoteRepository.cs
@@ -0,0 +1,13 @@
+﻿using QuotesTestAPI.Interfacecs;
+using QuotesTestAPI.Models;
+
+namespace QuotesTestAPI.Repository
+{
+    public class QuoteRepository : IQuotesRepository
+    {
+        public ICollection<Quote> GetQuotes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
