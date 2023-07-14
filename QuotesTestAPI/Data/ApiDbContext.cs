e7f337718e951c7e4d04c7338f23f6bd1e77aff7
todor-grigorov <t.grigorov99@gmail.com>
2023-07-14 21:55:16 +0200

    feat: add ApiDbContext

---
+++ QuotesTestAPI/Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuotesTestAPI.Models;
+
+namespace QuotesTestAPI.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Quote> Quotes { get; set; }
+    }
+}
