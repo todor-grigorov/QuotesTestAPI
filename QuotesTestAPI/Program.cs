76c1529768cc9a82b3f8d8fd2823f0c2878314ea
todor-grigorov <t.grigorov99@gmail.com>
2023-07-15 00:06:46 +0200

    feat: add QuoteRepository to builder

--- QuotesTestAPI/Program.cs
+++ QuotesTestAPI/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using QuotesTestAPI.Data;
+using QuotesTestAPI.Interfacecs;
+using QuotesTestAPI.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IQuotesRepository, QuoteRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
