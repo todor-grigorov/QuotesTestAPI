b668e1054cc605ab9e58ea119ef594f856a472ca
todor-grigorov <t.grigorov99@gmail.com>
2023-07-15 00:04:19 +0200

    feat: import db context in QuoteRepository

--- QuotesTestAPI/Repository/QuoteRepository.cs
+++ QuotesTestAPI/Repository/QuoteRepository.cs
@@ -1,10 +1,18 @@
-﻿using QuotesTestAPI.Interfacecs;
+﻿using QuotesTestAPI.Data;
+using QuotesTestAPI.Interfacecs;
 using QuotesTestAPI.Models;
 
 namespace QuotesTestAPI.Repository
 {
     public class QuoteRepository : IQuotesRepository
     {
+        private readonly ApiDbContext _context;
+
+        public QuoteRepository(ApiDbContext context)
+        {
+            _context = context;
+        }
+
         public ICollection<Quote> GetQuotes()
         {
             throw new NotImplementedException();
