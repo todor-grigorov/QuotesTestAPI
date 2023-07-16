556edbba14b1259ba96faf1e75660c043d125d9a
todor-grigorov <t.grigorov99@gmail.com>
2023-07-16 21:24:56 +0200

    feat: add QuotesController

---
+++ QuotesTestAPI/Controllers/QuotesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuotesTestAPI.Interfacecs;
+
+namespace QuotesTestAPI.Controllers
+{
+    public class QuotesController : Controller
+    {
+        private readonly IQuotesRepository _quotesRepository;
+
+        public QuotesController(IQuotesRepository quotesRepository)
+        {
+            _quotesRepository = quotesRepository;
+        }
+
+        
+    }
+}
