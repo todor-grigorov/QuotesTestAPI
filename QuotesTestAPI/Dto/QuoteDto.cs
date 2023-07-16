b070ecd492cf1798f5762a9eb8bb681ed386337d
todor-grigorov <t.grigorov99@gmail.com>
2023-07-16 21:31:53 +0200

    feat: add Quote DTO

---
+++ QuotesTestAPI/Dto/QuoteDto.cs
@@ -0,0 +1,10 @@
+﻿namespace QuotesTestAPI.Dto
+{
+    public class QuoteDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
