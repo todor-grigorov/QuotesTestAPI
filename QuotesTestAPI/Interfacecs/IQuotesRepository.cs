2ff4858daf3dcb716f0721071f5dd208dadee424
todor-grigorov <t.grigorov99@gmail.com>
2023-07-22 21:57:18 +0200

    feat: add QuoteExists in IQuotesRepository

--- QuotesTestAPI/Interfacecs/IQuotesRepository.cs
+++ QuotesTestAPI/Interfacecs/IQuotesRepository.cs
@@ -8,6 +8,7 @@ namespace QuotesTestAPI.Interfacecs
         Quote GetQuotesById(int id);
         void CreateQuote(Quote quote);
         void UpdateQuote(Quote quote);
+        bool QuoteExists(int id);
         bool Save();
     }
 }
