e617def3f4262796c8deb06fc4feed68d0a044f3
todor-grigorov <t.grigorov99@gmail.com>
2023-08-13 23:57:35 +0200

    feat: add Sort static class

---
+++ QuotesTestAPI/Common/Sort.cs
@@ -0,0 +1,8 @@
+﻿namespace QuotesTestAPI.Common
+{
+    public static class Sort
+    {
+        public static readonly string Ascending = "asc";
+        public static readonly string Descending = "desc";
+    }
+}
