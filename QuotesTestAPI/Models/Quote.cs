19cb6a34938ac3dc8f3a51e6d8434b6ef49f4431
todor-grigorov <t.grigorov99@gmail.com>
2023-08-18 20:05:20 +0200

    refactor: add type prop to Quote model

--- QuotesTestAPI/Models/Quote.cs
+++ QuotesTestAPI/Models/Quote.cs
@@ -6,6 +6,7 @@
         public string Title { get; set; }
         public string Author { get; set; }
         public string Description { get; set; }
+        public string Type { get; set; }
         public string CreatedAt { get; set; }
     }
 }
