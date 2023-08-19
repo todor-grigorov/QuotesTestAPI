6338e1970da8c014928e9315c15b337fc7715286
todor-grigorov <t.grigorov99@gmail.com>
2023-08-19 20:13:39 +0200

    chore: db migrations

---
+++ QuotesTestAPI/Migrations/20230830180854_Add-Type-Prop.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuotesTestAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTypeProp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Quotes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Quotes");
+        }
+    }
+}
